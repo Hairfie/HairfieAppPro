401634e778eff3d577956136c29f54f3805f5caa
Antoine Hérault <antoine.herault@gmail.com>
2015-06-15 12:39:06 +0200

    Add logo to login scene - close #2

--- src/scenes/login-scene.js
+++ src/scenes/login-scene.js
@@ -1,9 +1,15 @@
 'use strict';
 
 import React from 'react-native';
-import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import {
+    Component,
+    StyleSheet,
+    View,
+    Text,
+    TouchableHighlight,
+    Image
+} from 'react-native';
 import t from 'tcomb-form-native';
-import FacebookLoginButton from '../components/facebook-login-button';
 
 const { Form } = t.form;
 
@@ -13,12 +19,14 @@ const Credentials = t.struct({
 });
 
 const credentialsOptions = {
+    auto: 'placeholders',
     fields: {
         email: {
             autoCapitalize: 'none',
             keyboardType: 'email-address'
         },
         password: {
+            placeholder: 'Mot de passe',
             password: true
         }
     }
@@ -30,6 +38,10 @@ const styles = StyleSheet.create({
         marginTop: 65,
         padding: 10
     },
+    logo: {
+        height: 50,
+        margin: 20
+    },
     submitButton: {
         backgroundColor: '#fe5b5f',
         padding: 10
@@ -39,15 +51,24 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
     facebookLogin: {
-        paddingTop: 10
+        paddingTop: 10,
+        borderRadius: 5
     },
     facebookLoginTitle: {
         textAlign: 'center',
-        padding: 30
+        padding: 20
+    },
+    facebookLoginButton: {
+        backgroundColor: '#3b5998',
+        padding: 10
+    },
+    facebookLoginButtonText: {
+        color: '#ffffff',
+        textAlign: 'center'
     }
 });
 
-class LoginScene extends React.Component {
+class LoginScene extends Component {
 
     static contextTypes = {
         flux: React.PropTypes.object
@@ -56,31 +77,39 @@ class LoginScene extends React.Component {
     render() {
         return (
             <View style={styles.container}>
+                <Image
+                    style={styles.logo}
+                    resizeMode="contain"
+                    source={require('image!LoginLogo')}
+                />
                 <Form
                     ref="form"
                     type={Credentials}
                     options={credentialsOptions}
                 />
-                {this.renderSubmitButton()}
+                <TouchableHighlight
+                    style={styles.submitButton}
+                    onPress={this._handleSubmit}
+                    underlayColor='#99d9f4'
+                >
+                    <Text style={styles.submitButtonText}>
+                        {this.props.loggingIn ? 'Connexion en cours...' : 'Se connecter'}
+                    </Text>
+                </TouchableHighlight>
                 <View style={styles.facebookLogin}>
-                    <Text style={styles.facebookLoginTitle}>ou</Text>
-                    <FacebookLoginButton />
+                    <Text style={styles.facebookLoginTitle}>
+                        ou
+                    </Text>
+                    <TouchableHighlight
+                        style={styles.facebookLoginButton}
+                        onPress={this._handleFacebookLoginPress}
+                    >
+                        <Text style={styles.facebookLoginButtonText}>
+                            Se connecter via Facebook
+                        </Text>
+                    </TouchableHighlight>
                 </View>
             </View>
-        );
-    }
-
-    renderSubmitButton() {
-        return (
-            <TouchableHighlight
-                style={styles.submitButton}
-                onPress={this._handleSubmit}
-                underlayColor='#99d9f4'
-            >
-                <Text style={styles.submitButtonText}>
-                    {this.props.loggingIn ? 'Connexion en cours...' : 'Se connecter'}
-                </Text>
-            </TouchableHighlight>
         );
     }
 
@@ -89,6 +118,10 @@ class LoginScene extends React.Component {
         var credentials = this.refs.form.getValue();
         if (!credentials) return;
         this.context.flux.actions.auth.submitLoginForm(credentials.email, credentials.password);
+    }
+
+    _handleFacebookLoginPress = () => {
+        this.context.flux.actions.auth.loginWithFacebook();
     }
 }
 
